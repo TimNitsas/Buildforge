df6e3d8fbe3678fc21828da075c3115d270566a6
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-15 13:55:06 -0400

    init

---
+++ Buildforge.App/App.xaml.cs
@@ -0,0 +1,7 @@
+﻿using System.Windows;
+
+namespace Buildforge.App;
+
+public partial class App : Application
+{
+}
---
+++ Buildforge.App/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Buildforge.App;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
