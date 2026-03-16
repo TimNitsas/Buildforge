62c5d88d4e83d732a281fc7dd7d6ebb01d54d7e6
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-16 15:42:07 -0400

    Create MainViewModel.cs

---
+++ Buildforge.App/ViewModel/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Buildforge.App.ViewModel;
+
+public partial class MainViewModel
+{
+    public string? Version { get; }
+
+    public MainViewModel()
+    {
+        Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
+    }
+}
