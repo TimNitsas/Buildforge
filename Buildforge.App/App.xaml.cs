571b936353ed7660a607ba88594c3d5aae5591b7
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-16 14:06:11 -0400

    Update App.xaml.cs

--- Buildforge.App/App.xaml.cs
+++ Buildforge.App/App.xaml.cs
@@ -1,7 +1,19 @@
 ﻿using System.Windows;
+using Velopack;
 
 namespace Buildforge.App;
 
 public partial class App : Application
 {
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        VelopackApp.Build().Run();
+
+        App app = new();
+
+        app.InitializeComponent();
+
+        app.Run();
+    }
 }
