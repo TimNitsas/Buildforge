d3a1640e56090b635f1534bb39fc7b3d6181fbe2
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-17 10:33:05 -0400

    Build view

---
+++ Buildforge.App/View/BuildView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Buildforge.App.View;
+
+public partial class BuildView : UserControl
+{
+    public BuildView()
+    {
+        InitializeComponent();
+    }
+}
