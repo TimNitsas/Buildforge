ada8b35a3d4fa9d52b2d0544d9c2ded0afa3d0e4
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-17 10:08:44 -0400

    Update App.xaml.cs

--- Buildforge.App/App.xaml.cs
+++ Buildforge.App/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Buildforge.App.ViewModel;
 using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
 using Velopack;
+using Velopack.Exceptions;
+using Velopack.Sources;
 
 namespace Buildforge.App;
 
@@ -17,14 +20,67 @@ public partial class App : Application
         Services = services.BuildServiceProvider();
     }
 
+    private static async Task UpdateLoop(CancellationToken ct)
+    {
+        var source = new GithubSource("https://github.com/TimNitsas/Buildforge", null, false);
+
+        var updateManager = new UpdateManager(source);
+
+        while (!ct.IsCancellationRequested)
+        {
+            UpdateInfo? newVersion;
+
+            try
+            {
+                newVersion = await updateManager.CheckForUpdatesAsync();
+            }
+            catch (NotInstalledException)
+            {
+                if (Debugger.IsAttached)
+                {
+                    return;
+                }
+
+                throw;
+            }
+
+            if (newVersion != null)
+            {
+                await updateManager.DownloadUpdatesAsync(newVersion, cancelToken: ct);
+            }
+
+            await Task.Delay(TimeSpan.FromMinutes(5), ct);
+        }
+    }
+
     [STAThread]
     private static void Main(string[] args)
     {
         VelopackApp.Build().Run();
 
+        using var cts = new CancellationTokenSource();
+
+        var updateAppTask = Task.Run(async () =>
+        {
+            await UpdateLoop(cts.Token);
+        });
+
         App app = new();
 
         app.InitializeComponent();
+
+        app.Exit += async (sender, e) =>
+        {
+            cts.Cancel();
+
+            try
+            {
+                await updateAppTask;
+            }
+            catch (OperationCanceledException)
+            {
+            }
+        };
 
         app.Run();
     }
