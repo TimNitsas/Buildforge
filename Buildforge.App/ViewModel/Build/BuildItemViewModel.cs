fcdc434c629e44ab1ba188746e33caa3a6597cb1
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-17 10:50:31 -0400

    Build view models

---
+++ Buildforge.App/ViewModel/Build/BuildItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ApiSdk.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Buildforge.App.ViewModel;
+
+public partial class BuildItemViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string? name;
+
+    public BuildItemViewModel(Build build)
+    {
+        Name = build.Name;
+    }
+}
---
+++ Buildforge.App/ViewModel/Build/BuildViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace Buildforge.App.ViewModel;
+
+public partial class BuildViewModel : ObservableObject
+{
+    public ObservableCollection<BuildItemViewModel> Builds { get; } = [];
+}
