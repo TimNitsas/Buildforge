341f790cbca75332af9fc4931851d09e4dca631e
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-21 13:50:01 -0400

    Create SayHelloCli.cs

---
+++ Buildforge.App/Cli/SayHelloCli.cs
@@ -0,0 +1,16 @@
+﻿using DotMake.CommandLine;
+using System.Windows.Threading;
+
+namespace Buildforge.App.Cli;
+
+[CliCommand(Name = "sayhello", Description = "Says hello", Parent = typeof(RootCli))]
+public class SayHelloCli
+{
+    public void Run()
+    {
+        Dispatcher.CurrentDispatcher.Invoke(() =>
+        {
+            MessageBox.Show("Hello!");
+        });
+    }
+}
