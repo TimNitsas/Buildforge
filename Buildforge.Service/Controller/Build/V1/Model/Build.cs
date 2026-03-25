47842f9b1a2dcac4ea85756d155553e23f139901
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-24 21:41:33 -0400

    Update Build.cs

--- Buildforge.Service/Controller/Build/V1/Model/Build.cs
+++ Buildforge.Service/Controller/Build/V1/Model/Build.cs
@@ -3,4 +3,8 @@
 public sealed class Build
 {
     public required string Name { get; init; }
+
+    public required string Target { get; init; }
+
+    public required BuildStatus Status { get; init; }
 }
