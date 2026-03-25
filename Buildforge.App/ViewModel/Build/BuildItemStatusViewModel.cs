cb6e22f9cc206016b8a39917130d8d85bd710838
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-25 19:58:04 -0400

    Create BuildItemStatusViewModel.cs

---
+++ Buildforge.App/ViewModel/Build/BuildItemStatusViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Buildforge.App.Messaging;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace Buildforge.App.ViewModel.Build;
+
+public abstract partial class BuildItemStatusViewModel : ObservableObject, IRecipient<TickTimeMessage>
+{
+    [ObservableProperty]
+    private DateTime startTime;
+
+    protected BuildItemStatusViewModel(Client.V1.BuildStatus status)
+    {
+        StartTime = status.StartTime.Date;
+
+        WeakReferenceMessenger.Default.RegisterAll(this);
+    }
+
+    public void Receive(TickTimeMessage message)
+    {
+        OnPropertyChanged(nameof(StartTime));
+    }
+}
