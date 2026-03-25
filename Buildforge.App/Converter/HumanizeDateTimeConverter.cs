c92347465ac75bc3ff7f675ee0bf4e943c87e5ca
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-24 21:50:32 -0400

    Create HumanizeDateTimeConverter.cs

---
+++ Buildforge.App/Converter/HumanizeDateTimeConverter.cs
@@ -0,0 +1,21 @@
+﻿using Humanizer;
+
+namespace Buildforge.App.Converter;
+
+public sealed class HumanizeDateTimeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is DateTime dt)
+        {
+            return dt.Humanize();
+        }
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
