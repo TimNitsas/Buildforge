2f4dc9cc5457727893a696915b6bf2161a3f79b2
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-24 21:50:52 -0400

    Update BuildViewModelDesignTime.cs

--- Buildforge.App/ViewModel/Build/BuildViewModelDesignTime.cs
+++ Buildforge.App/ViewModel/Build/BuildViewModelDesignTime.cs
@@ -1,10 +1,14 @@
 ﻿using Buildforge.Client.V1;
 
-namespace Buildforge.App.ViewModel;
+namespace Buildforge.App.ViewModel.Build;
 
 public sealed class BuildViewModelDesignTime : BuildViewModel
 {
     public BuildViewModelDesignTime() : base(new MockBuildforgeClient())
     {
+        foreach (var build in MockBuildforgeClient.GetBuildResult().Builds)
+        {
+            Builds.Add(new BuildItemViewModel(build));
+        }
     }
 }
