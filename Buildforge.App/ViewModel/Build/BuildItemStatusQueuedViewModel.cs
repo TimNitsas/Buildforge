23ace94ebf4085906283dc0c3b12180f52634346
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-25 19:58:49 -0400

    Create BuildItemStatusQueuedViewModel.cs

---
+++ Buildforge.App/ViewModel/Build/BuildItemStatusQueuedViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Buildforge.App.ViewModel.Build;
+
+public partial class BuildItemStatusQueuedViewModel : BuildItemStatusViewModel
+{
+    public BuildItemStatusQueuedViewModel(Client.V1.BuildStatusQueued status) : base(status)
+    {
+    }
+}
