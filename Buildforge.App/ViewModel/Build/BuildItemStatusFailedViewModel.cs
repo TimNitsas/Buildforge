415a69b5b234f1dde3a0a0279d7a48eede879e6d
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-25 19:58:54 -0400

    Create BuildItemStatusFailedViewModel.cs

---
+++ Buildforge.App/ViewModel/Build/BuildItemStatusFailedViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Buildforge.App.ViewModel.Build;
+
+public partial class BuildItemStatusFailedViewModel : BuildItemStatusViewModel
+{
+    [ObservableProperty]
+    private string? reason;
+
+    public BuildItemStatusFailedViewModel(Client.V1.BuildStatusFailed status) : base(status)
+    {
+        Reason = status.Reason;
+    }
+}
