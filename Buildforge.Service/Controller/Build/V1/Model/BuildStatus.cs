f575a438cbea9a5aeda1242dd3a9e424f1e6eaab
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-24 21:49:21 -0400

    Update BuildStatus.cs

--- Buildforge.Service/Controller/Build/V1/Model/BuildStatus.cs
+++ Buildforge.Service/Controller/Build/V1/Model/BuildStatus.cs
@@ -3,8 +3,10 @@
 namespace Buildforge.Service.Controller.Build.V1.Model;
 
 [JsonPolymorphic(TypeDiscriminatorPropertyName = "discriminator")]
-[JsonDerivedType(typeof(BuildStatusFailed))]
-[JsonDerivedType(typeof(BuildStateSuccess))]
+[JsonDerivedType(typeof(BuildStatusFailed), typeDiscriminator: nameof(BuildStatusFailed))]
+[JsonDerivedType(typeof(BuildStatusSuccess), typeDiscriminator: nameof(BuildStatusSuccess))]
+[JsonDerivedType(typeof(BuildStatusQueued), typeDiscriminator: nameof(BuildStatusQueued))]
 public abstract class BuildStatus
 {
+    public DateTime StartTime { get; set; }
 }
