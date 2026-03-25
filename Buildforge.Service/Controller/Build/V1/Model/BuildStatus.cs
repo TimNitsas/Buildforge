01115abba6405e2ba8f4dcc5fc14101b4e5a6b09
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-25 19:57:25 -0400

    Update BuildStatus.cs

--- Buildforge.Service/Controller/Build/V1/Model/BuildStatus.cs
+++ Buildforge.Service/Controller/Build/V1/Model/BuildStatus.cs
@@ -6,6 +6,7 @@ namespace Buildforge.Service.Controller.Build.V1.Model;
 [JsonDerivedType(typeof(BuildStatusFailed), typeDiscriminator: nameof(BuildStatusFailed))]
 [JsonDerivedType(typeof(BuildStatusSuccess), typeDiscriminator: nameof(BuildStatusSuccess))]
 [JsonDerivedType(typeof(BuildStatusQueued), typeDiscriminator: nameof(BuildStatusQueued))]
+[JsonDerivedType(typeof(BuildStatusActive), typeDiscriminator: nameof(BuildStatusActive))]
 public abstract class BuildStatus
 {
     public DateTime StartTime { get; set; }
