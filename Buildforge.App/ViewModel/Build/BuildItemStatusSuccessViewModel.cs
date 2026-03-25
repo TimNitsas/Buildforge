e8f923cb4f36fe010825f4de5bf4d6e6f1e7d050
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-25 19:58:47 -0400

    Create BuildItemStatusSuccessViewModel.cs

---
+++ Buildforge.App/ViewModel/Build/BuildItemStatusSuccessViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Buildforge.App.ViewModel.Build;
+
+public partial class BuildItemStatusSuccessViewModel : BuildItemStatusViewModel
+{
+    [ObservableProperty]
+    private TimeSpan? buildTime;
+
+    public BuildItemStatusSuccessViewModel(Client.V1.BuildStatusSuccess status) : base(status)
+    {
+        BuildTime = status.BuildTime;
+    }
+}
