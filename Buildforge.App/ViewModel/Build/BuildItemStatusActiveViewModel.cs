d8d584d447a712e7870456913306bc664cebd052
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-25 19:58:57 -0400

    Create BuildItemStatusActiveViewModel.cs

---
+++ Buildforge.App/ViewModel/Build/BuildItemStatusActiveViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Buildforge.App.ViewModel.Build;
+
+public partial class BuildItemStatusActiveViewModel : BuildItemStatusViewModel
+{
+    public BuildItemStatusActiveViewModel(Client.V1.BuildStatusActive status) : base(status)
+    {
+    }
+}
