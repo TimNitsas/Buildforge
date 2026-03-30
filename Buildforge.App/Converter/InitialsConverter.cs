3d5554fe685e8d5f789481780ade032ea1e999d6
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-30 19:56:40 -0400

    Create InitialsConverter.cs

---
+++ Buildforge.App/Converter/InitialsConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace Buildforge.App.Converter;
+
+public class InitialsConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        string? input = value as string;
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return string.Empty;
+        }
+
+        var words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        var capitalize = words.Select(s => s.ToUpper(culture)).Select(word => word[0]);
+
+        return string.Join(string.Empty, capitalize);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
