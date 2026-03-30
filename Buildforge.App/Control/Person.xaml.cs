31e865233cbc604517046bfb77d26c74a4c43e67
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-30 19:58:41 -0400

    Create Person.xaml.cs

---
+++ Buildforge.App/Control/Person.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace Buildforge.App.Control;
+
+public partial class Person : UserControl
+{
+    public string PersonName
+    {
+        get { return (string)GetValue(PersonNameProperty); }
+        set { SetValue(PersonNameProperty, value); }
+    }
+
+    public static readonly DependencyProperty PersonNameProperty = DependencyProperty.Register(nameof(PersonName), typeof(string), typeof(Person), new PropertyMetadata(string.Empty));
+
+    public Person()
+    {
+        InitializeComponent();
+    }
+}
