22b90fbcc666d233b7b51a9d2478db4e96cfa80b
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-30 19:56:50 -0400

    Create BuildCrash.cs

---
+++ Buildforge.Service/Controller/Build/V1/Model/BuildCrash.cs
@@ -0,0 +1,10 @@
+﻿namespace Buildforge.Service.Controller.Build.V1.Model;
+
+public sealed class BuildCrash
+{
+    public required string User { get; init; }
+
+    public required DateTime Date { get; init; }
+
+    public required TimeSpan PlayTime { get; init; }
+}
