c8868fde7afd25415b834d8ad0eee01f01bdf2c2
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-03-30 19:56:28 -0400

    Create HumanizeBytesConverter.cs

---
+++ Buildforge.App/Converter/HumanizeBytesConverter.cs
@@ -0,0 +1,21 @@
+﻿using Humanizer;
+
+namespace Buildforge.App.Converter;
+
+public sealed class HumanizeBytesConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is long dt)
+        {
+            return dt.Bytes().Humanize();
+        }
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
