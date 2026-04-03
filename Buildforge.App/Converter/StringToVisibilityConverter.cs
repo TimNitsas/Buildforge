a0359f43cc5132f90587ff27aa6c2939771d6508
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-03 14:48:46 -0400

    Create StringToVisibilityConverter.cs

---
+++ Buildforge.App/Converter/StringToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace Buildforge.App.Converter;
+
+public class StringToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (string.IsNullOrWhiteSpace(value as string))
+        {
+            return Visibility.Collapsed;
+        }
+        else
+        {
+            return Visibility.Visible;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
