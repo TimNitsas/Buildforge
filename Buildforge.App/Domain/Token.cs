c4ff1473e4e29d1bef14c4a14b440d323c28fda4
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-03 09:50:43 -0400

    Create Token.cs

---
+++ Buildforge.App/Domain/Token.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Buildforge.App.Domain;
+
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "version")]
+[JsonDerivedType(typeof(V1), "v1")]
+public abstract class Token
+{
+    public sealed class V1 : Token
+    {
+        public required string Payload { get; set; }
+
+        public DateTime UtcExpiry { get; set; }
+    }
+}
