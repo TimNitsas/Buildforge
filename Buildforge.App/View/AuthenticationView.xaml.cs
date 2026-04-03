7fa037f28ced13b9417cba591e56e2781fd9c32a
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-03 09:57:32 -0400

    Create AuthenticationView.xaml.cs

---
+++ Buildforge.App/View/AuthenticationView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Buildforge.App.Domain;
+using Buildforge.App.ViewModel.Authentication;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.Wpf;
+
+namespace Buildforge.App.View;
+
+public partial class AuthenticationView : UserControl
+{
+    public AuthenticationView()
+    {
+        InitializeComponent();
+
+        DataContext = App.Services.GetRequiredService<AuthenticationViewModel>();
+    }
+
+    private async void WebViewInitialized(object sender, EventArgs e)
+    {
+        if (App.Services.GetRequiredService<TokenHandler>().HasValidToken())
+        {
+            return;
+        }
+
+        if (sender is not WebView2 webView)
+        {
+            return;
+        }
+
+        await webView.EnsureCoreWebView2Async();
+
+        await webView.CoreWebView2.Profile.ClearBrowsingDataAsync(CoreWebView2BrowsingDataKinds.AllProfile);
+    }
+
+    private void WebViewNavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+    {
+        if (sender is not WebView2 webView)
+        {
+            return;
+        }
+
+        if (DataContext is not AuthenticationViewModel authenticationViewModel)
+        {
+            return;
+        }
+
+        var uri = new Uri(e.Uri);
+
+        if (uri.AbsoluteUri.StartsWith(AuthenticationViewModel.RedirectUri))
+        {
+            e.Cancel = true;
+
+            Task.Run(async () =>
+            {
+                await authenticationViewModel.Authenticate(uri);
+            });
+        }
+    }
+}
