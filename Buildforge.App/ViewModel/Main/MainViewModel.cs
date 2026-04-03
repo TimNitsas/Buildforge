8de858fa784e26997a51d9a10965346ebef0667b
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-03 10:11:45 -0400

    Update MainViewModel.cs

--- Buildforge.App/ViewModel/Main/MainViewModel.cs
+++ Buildforge.App/ViewModel/Main/MainViewModel.cs
@@ -1,16 +1,40 @@
-﻿namespace Buildforge.App.ViewModel.Main;
+﻿using Buildforge.App.Domain;
+using Buildforge.App.Event;
+using Buildforge.App.ViewModel.Authentication;
+using CommunityToolkit.Mvvm.Messaging;
 
-public partial class MainViewModel
+namespace Buildforge.App.ViewModel.Main;
+
+public partial class MainViewModel : ObservableObject, IRecipient<TokenAcquiredEvent>
 {
     public string? Version { get; }
 
-    public MainViewModel()
+    public object CurrentViewModel => GetCurrentViewModel(TokenHandler);
+
+    private readonly TokenHandler TokenHandler;
+
+    public MainViewModel(TokenHandler tokenHandler)
     {
+        TokenHandler = tokenHandler;
+
+        WeakReferenceMessenger.Default.RegisterAll(this);
+
         Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
+
+        OnPropertyChanged(nameof(CurrentViewModel));
     }
-}
 
-public sealed class MainViewModelDesignTime : MainViewModel
-{
+    public void Receive(TokenAcquiredEvent message)
+    {
+        OnPropertyChanged(nameof(CurrentViewModel));
+    }
 
+    private static object GetCurrentViewModel(TokenHandler tokenHandler)
+    {
+        return tokenHandler.HasValidToken() switch
+        {
+            true => App.Services.GetRequiredService<BuildViewModel>(),
+            false => App.Services.GetRequiredService<AuthenticationViewModel>()
+        };
+    }
 }
