8b532ee4e556a41e1c6f5ad0a027e3255b0e910c
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-03 09:48:04 -0400

    Create AuthenticationResult.cs

---
+++ Buildforge.Service/Controller/Authentication/V1/AuthenticationResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Buildforge.Service.Controller.Authentication.V1;
+
+public sealed class AuthenticationResult
+{
+    public required string Jwt { get; set; }
+
+    public required string UserName { get; set; }
+
+    public DateTime UtcExpiry { get; set; }
+}
