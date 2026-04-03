fcabaccc4c63cf5fc90cb9b8685c86157ae507e5
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-03 09:40:42 -0400

    Create Authenticator.cs

---
+++ Buildforge.Service/Domain/Authenticator/Authenticator.cs
@@ -0,0 +1,24 @@
+﻿namespace Buildforge.Service.Domain.Authenticator;
+
+public enum AuthenticationPlatform
+{
+    None,
+    Github
+}
+
+public sealed class AuthenticatorResult
+{
+    public required string UserName { get; set; }
+}
+
+public sealed class AuthenticatorOptions
+{
+    [Required]
+    [MinLength(1)]
+    public required string SymmetricKey { get; set; }
+}
+
+public interface IAuthenticator
+{
+    Task<AuthenticatorResult> Authenticate(string code);
+}
