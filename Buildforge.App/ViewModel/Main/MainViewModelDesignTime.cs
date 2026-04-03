f5d0e2fbd23ce3009ca3a53d6ecaa7a9caccfd72
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-03 09:51:06 -0400

    Create MainViewModelDesignTime.cs

---
+++ Buildforge.App/ViewModel/Main/MainViewModelDesignTime.cs
@@ -0,0 +1,10 @@
+﻿using Buildforge.App.Domain;
+
+namespace Buildforge.App.ViewModel.Main;
+
+public sealed class MainViewModelDesignTime : MainViewModel
+{
+    public MainViewModelDesignTime() : base(new TokenHandler())
+    {
+    }
+}
