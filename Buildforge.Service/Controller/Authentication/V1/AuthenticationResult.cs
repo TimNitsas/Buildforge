ea90567b140b552b1e69b3343493cbfe733a225b
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-03 14:48:56 -0400

    Update AuthenticationResult.cs

--- Buildforge.Service/Controller/Authentication/V1/AuthenticationResult.cs
+++ Buildforge.Service/Controller/Authentication/V1/AuthenticationResult.cs
@@ -4,7 +4,7 @@ public sealed class AuthenticationResult
 {
     public required string Jwt { get; set; }
 
-    public required string UserName { get; set; }
+    public required string Username { get; set; }
 
     public DateTime UtcExpiry { get; set; }
 }
