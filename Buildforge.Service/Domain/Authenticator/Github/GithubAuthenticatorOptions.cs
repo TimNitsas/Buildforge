70c67dacbb2a1991f684ae213953ca26ad648214
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-03 09:41:20 -0400

    Create GithubAuthenticatorOptions.cs

---
+++ Buildforge.Service/Domain/Authenticator/Github/GithubAuthenticatorOptions.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Buildforge.Service.Domain.Authenticator.Github;
+
+public class GithubAuthenticatorOptions
+{
+    [Required]
+    [MinLength(1)]
+    public required string ClientSecret { get; set; }
+
+    [Required]
+    [MinLength(1)]
+    public required string RedirectUri { get; set; }
+
+    [Required]
+    [MinLength(1)]
+    public required string ClientId { get; set; }
+}
