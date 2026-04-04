290de9b0e754da644926cdf3482844d768126bce
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-04 14:25:27 -0400

    Create AuthenticationViewModelDesignTime.cs

---
+++ Buildforge.App/ViewModel/Authentication/AuthenticationViewModelDesignTime.cs
@@ -0,0 +1,13 @@
+﻿#if DEBUG
+using Buildforge.App.Domain;
+using Buildforge.Client.V1;
+
+namespace Buildforge.App.ViewModel.Authentication;
+
+public sealed class AuthenticationViewModelDesignTime : AuthenticationViewModel
+{
+    public AuthenticationViewModelDesignTime() : base(new Moq.Mock<IAuthenticationClient>().Object, new TokenHandler())
+    {
+    }
+}
+#endif
