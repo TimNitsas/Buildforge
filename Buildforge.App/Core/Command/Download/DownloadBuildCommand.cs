a79c102e01cc7dbb88f2335d0e3b1b46c4d8a047
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-07 22:11:41 -0400

    Create DownloadBuildCommand.cs

---
+++ Buildforge.App/Core/Command/Download/DownloadBuildCommand.cs
@@ -0,0 +1,17 @@
+﻿using Buildforge.App.Core.Command.Shared;
+using Buildforge.App.Domain.Build;
+
+namespace Buildforge.App.Core.Command.Download;
+
+public sealed class DownloadBuildCommand(string buildId) : SharedCommand($"download-build-{buildId}")
+{
+    protected override bool CanExecuteImpl()
+    {
+        return !App.Services.GetRequiredService<BuildLocator>().HasBuild(buildId);
+    }
+
+    public override Task ExecuteImpl()
+    {
+        return App.Services.GetRequiredService<BuildHandler>().DownloadBuild(buildId, Cts.Token);
+    }
+}
