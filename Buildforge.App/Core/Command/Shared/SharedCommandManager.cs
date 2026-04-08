f0120bed41ef5ff87c0d174b91d5ae43e1ab218b
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-07 22:12:45 -0400

    Create SharedCommandManager.cs

---
+++ Buildforge.App/Core/Command/Shared/SharedCommandManager.cs
@@ -0,0 +1,44 @@
+﻿namespace Buildforge.App.Core.Command.Shared;
+
+public class SharedCommandManager
+{
+    public static readonly SharedCommandManager Instance = new();
+
+    private readonly List<string> Keys = [];
+
+    public void Queue(SharedCommand sharedCommand)
+    {
+        Assert.EnsureUiThread();
+
+        if (Keys.Contains(sharedCommand.Key))
+        {
+            return;
+        }
+
+        Keys.Add(sharedCommand.Key);
+
+        CommandManager.InvalidateRequerySuggested();
+
+        Task.Run(async () =>
+        {
+            try
+            {
+                await sharedCommand.ExecuteImpl();
+            }
+            finally
+            {
+                Dispatcher.CurrentDispatcher.Invoke(() =>
+                {
+                    Keys.Remove(sharedCommand.Key);
+
+                    CommandManager.InvalidateRequerySuggested();
+                });
+            }
+        });
+    }
+
+    public bool CanExecute(SharedCommand sharedCommand)
+    {
+        return !Keys.Contains(sharedCommand.Key);
+    }
+}
