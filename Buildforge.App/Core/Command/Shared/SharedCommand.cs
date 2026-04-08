ead5ecd8e53431d8d4e1bd3baa9b5d7cda8f61a6
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-07 22:12:41 -0400

    Create SharedCommand.cs

---
+++ Buildforge.App/Core/Command/Shared/SharedCommand.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Input;
+
+namespace Buildforge.App.Core.Command.Shared;
+
+public abstract class SharedCommand : ICommand
+{
+    public string Key { get; }
+
+    public CancellationTokenSource Cts { get; }
+
+    protected SharedCommand(string key)
+    {
+        Key = key;
+
+        Cts = new CancellationTokenSource();
+    }
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        if (!SharedCommandManager.Instance.CanExecute(this))
+        {
+            return false;
+        }
+
+        return CanExecuteImpl();
+    }
+
+    public void Execute(object? parameter)
+    {
+        SharedCommandManager.Instance.Queue(this);
+    }
+
+    protected abstract bool CanExecuteImpl();
+
+    public abstract Task ExecuteImpl();
+}
