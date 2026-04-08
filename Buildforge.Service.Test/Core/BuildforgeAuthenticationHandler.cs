246bb4453166b7e5924d7ce36faf388bde657d1b
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-07 21:48:04 -0400

    Create BuildforgeAuthenticationHandler.cs

---
+++ Buildforge.Service.Test/Core/BuildforgeAuthenticationHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Buildforge.Service.Test.Core;
+
+public sealed class BuildforgeAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder) : AuthenticationHandler<AuthenticationSchemeOptions>(options, logger, encoder)
+{
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Name, "User")
+        };
+
+        var identity = new ClaimsIdentity(claims, "Test");
+
+        var principal = new ClaimsPrincipal(identity);
+
+        var ticket = new AuthenticationTicket(principal, "Test");
+
+        return Task.FromResult(AuthenticateResult.Success(ticket));
+    }
+}
