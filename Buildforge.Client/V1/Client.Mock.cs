aa762d3512d9b3287a0e5c22b22cf17842aedf46
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-07 22:15:35 -0400

    Update Client.Mock.cs

--- Buildforge.Client/V1/Client.Mock.cs
+++ Buildforge.Client/V1/Client.Mock.cs
@@ -109,3 +109,16 @@ public partial class MockBuildforgeClient : IBuildClient
         throw new NotImplementedException();
     }
 }
+
+public partial class MockAuthenticationClient : IAuthenticationClient
+{
+    public Task<AuthenticationResult> GetTokenAsync(string? code = null, AuthenticationPlatform? authenticationPlatform = null, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(new AuthenticationResult()
+        {
+            Jwt = string.Empty,
+            Username = "fake-user",
+            UtcExpiry = DateTime.UtcNow.AddDays(1),
+        });
+    }
+}
