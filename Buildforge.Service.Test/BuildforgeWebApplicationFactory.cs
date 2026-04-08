e4e7f3d7005ca9b9ed16437549ab6898037118f1
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-07 21:15:10 -0400

    Create BuildforgeWebApplicationFactory.cs

---
+++ Buildforge.Service.Test/BuildforgeWebApplicationFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace Buildforge.Service.Test;
+
+public class BuildforgeWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    private const string Scheme = "Test";
+
+    protected override void ConfigureWebHost(Microsoft.AspNetCore.Hosting.IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.AddAuthentication(Scheme).AddScheme<AuthenticationSchemeOptions, BuildforgeAuthenticationHandler>(Scheme, options => { });
+        });
+    }
+}
