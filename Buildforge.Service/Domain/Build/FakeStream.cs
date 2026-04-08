8f2de630433e5f1a3012e67833d8456212531cba
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-07 21:52:51 -0400

    Create FakeStream.cs

---
+++ Buildforge.Service/Domain/Build/FakeStream.cs
@@ -0,0 +1,63 @@
+﻿namespace Buildforge.Service.Domain.Build;
+
+public class FakeStream(int length) : Stream
+{
+    private readonly Random Random = new();
+
+    public override bool CanRead => throw new NotImplementedException();
+
+    public override bool CanSeek => false;
+
+    public override bool CanWrite => throw new NotImplementedException();
+
+    public override long Length => throw new NotImplementedException();
+
+    public override long Position { get; set; }
+
+    public override void Flush()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+    {
+        if (Position >= length)
+        {
+            return 0;
+        }
+
+        int remaining = (int)Math.Min(buffer.Length, length - Position);
+
+        await Task.Delay(Random.Next(0, 10), cancellationToken);
+
+        byte[] temp = new byte[remaining];
+
+        Random.NextBytes(temp);
+
+        temp.CopyTo(buffer);
+
+        Position += remaining;
+
+        return remaining;
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void SetLength(long value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        throw new NotImplementedException();
+    }
+}
