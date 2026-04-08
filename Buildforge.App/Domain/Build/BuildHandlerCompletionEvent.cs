90acb566bd7425c494f81f30d10fd0291c1f97fc
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-07 22:12:55 -0400

    Create BuildHandlerCompletionEvent.cs

---
+++ Buildforge.App/Domain/Build/BuildHandlerCompletionEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Buildforge.App.Domain.Build;
+
+public sealed class BuildHandlerCompletionEvent
+{
+    public required TimeSpan Time { get; init; }
+
+    public required string Id { get; init; }
+}
