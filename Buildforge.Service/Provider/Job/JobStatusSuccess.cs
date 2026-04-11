cb4dda4bd8339a60863850f683cc04f0bbc4a77b
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-11 15:12:22 -0400

    Create JobStatusSuccess.cs

---
+++ Buildforge.Service/Provider/Job/JobStatusSuccess.cs
@@ -0,0 +1,8 @@
+﻿namespace Buildforge.Service.Provider.Job;
+
+public sealed class JobStatusSuccess : BuildStatus
+{
+    public required TimeSpan BuildTime { get; init; }
+
+    public required long Bytes { get; init; }
+}
