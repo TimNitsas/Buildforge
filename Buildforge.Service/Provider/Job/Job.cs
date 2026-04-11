e52ceadb38b975c43ff2a3579db6212a274756c9
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-11 15:12:05 -0400

    Create Job.cs

---
+++ Buildforge.Service/Provider/Job/Job.cs
@@ -0,0 +1,18 @@
+﻿namespace Buildforge.Service.Provider.Job;
+
+public sealed class Job
+{
+    public required DateTime StartTime { get; init; }
+
+    public required JobStatus Status { get; init; }
+
+    public required string Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public required string Target { get; set; }
+
+    public required string Platform { get; set; }
+
+    public required string Branch { get; set; }
+}
