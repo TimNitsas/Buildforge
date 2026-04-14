83dd4ba74127e9cb68b381919a69c81a7185c61b
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:32:20 -0400

    Create BuildCreatedEvent.cs

---
+++ Buildforge.Service/Controller/Event/V1/Model/BuildCreatedEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace Buildforge.Service.Controller.Event.V1.Model;
+
+public sealed class BuildCreatedEvent : Event
+{
+    public required Controller.Build.V1.Model.Build Data { get; set; }
+
+    public static BuildCreatedEvent FromDomain(Domain.Event.Model.BuildCreatedEvent e)
+    {
+        return new BuildCreatedEvent()
+        {
+            Data = Controller.Build.V1.Model.Build.FromDomain(e.Build)
+        };
+    }
+}
