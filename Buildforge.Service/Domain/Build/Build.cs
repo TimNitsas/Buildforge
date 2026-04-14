9eef9a01a7c069149aa7902d58916fc9eb5a5259
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:33:26 -0400

    Create Build.cs

---
+++ Buildforge.Service/Domain/Build/Build.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Buildforge.Service.Domain.Build;
+
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "Version")]
+[JsonDerivedType(typeof(V1.Build), "v1")]
+public abstract class Build
+{
+    public required string Id { get; set; }
+
+    public required DateTime ReadAt { get; set; }
+}
