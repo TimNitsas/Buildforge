0b893d00dcb412b2924fdc8a92ffe9662dd4a42f
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:16:24 -0400

    Create BuildStatus.cs

---
+++ Buildforge.Service/Domain/Build/V1/BuildStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Buildforge.Service.Domain.Build.V1;
+
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "Type")]
+[JsonDerivedType(typeof(BuildStatusActive), "active")]
+[JsonDerivedType(typeof(BuildStatusFailed), "failed")]
+[JsonDerivedType(typeof(BuildStatusQueued), "queued")]
+[JsonDerivedType(typeof(BuildStatusSuccess), "success")]
+public abstract class BuildStatus
+{
+    public DateTime StartTime { get; set; }
+}
