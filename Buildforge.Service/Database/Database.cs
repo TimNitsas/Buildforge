dc915e39461338ce4e6dde10e2901e5bab6031ad
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:21:48 -0400

    Create Database.cs

---
+++ Buildforge.Service/Database/Database.cs
@@ -0,0 +1,18 @@
+﻿using Npgsql;
+
+namespace Buildforge.Service.Database;
+
+public sealed class Database : IAsyncDisposable
+{
+    public NpgsqlDataSource DataSource { get; }
+
+    public Database(IOptions<DatabaseOptions> databaseOption)
+    {
+        DataSource = NpgsqlDataSource.Create(databaseOption.Value.ToConnectionString());
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await DataSource.DisposeAsync();
+    }
+}
