c710405a3379d352a19cd12619044902a940c575
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:16:52 -0400

    Create BuildStatusSuccess.cs

---
+++ Buildforge.Service/Domain/Build/V1/BuildStatusSuccess.cs
@@ -0,0 +1,8 @@
+﻿namespace Buildforge.Service.Domain.Build.V1;
+
+public sealed class BuildStatusSuccess : BuildStatus
+{
+    public required TimeSpan BuildTime { get; init; }
+
+    public required long Bytes { get; init; }
+}
