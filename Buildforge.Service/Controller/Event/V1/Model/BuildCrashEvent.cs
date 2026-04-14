b8f3bedf0a4c44271a13b08838ab96714fbdc510
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:32:15 -0400

    Create BuildCrashEvent.cs

---
+++ Buildforge.Service/Controller/Event/V1/Model/BuildCrashEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace Buildforge.Service.Controller.Event.V1.Model;
+
+public sealed class BuildCrashEvent : Event
+{
+    public required Build.V1.Model.BuildCrash Data { get; set; }
+
+    public static BuildCrashEvent FromDomain(Domain.Event.Model.BuildCrashedEvent c)
+    {
+        return new BuildCrashEvent()
+        {
+            Data = new Build.V1.Model.BuildCrash()
+            {
+                BuildId = c.Data.BuildId,
+                Date = c.Data.Date,
+                PlayTime = c.Data.PlayTime,
+                User = c.Data.User
+            }
+        };
+    }
+}
