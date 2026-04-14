5d44c57545dec18e9d48b39beed8b03dbef013e5
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 21:08:30 -0400

    Create Contribution.cs

---
+++ Buildforge.Service/Provider/Contribution/Contribution.cs
@@ -0,0 +1,10 @@
+﻿namespace Buildforge.Service.Provider.Contribution;
+
+public sealed class Contribution
+{
+    public required string Id { get; init; }
+
+    public required string User { get; init; }
+
+    public required string Description { get; init; }
+}
