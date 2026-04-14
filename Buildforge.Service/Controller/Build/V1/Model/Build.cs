b627b397a14f0eb4881c564fec6e1121f9821b5a
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:24:39 -0400

    Update Build.cs

--- Buildforge.Service/Controller/Build/V1/Model/Build.cs
+++ Buildforge.Service/Controller/Build/V1/Model/Build.cs
@@ -12,11 +12,32 @@ public sealed class Build
 
     public required string Branch { get; init; }
 
+    public required List<string> Tags { get; init; }
+
     public required BuildStatus Status { get; init; }
 
     public required List<BuildContribution> Contributions { get; init; }
 
     public required List<BuildCrash> Crashes { get; init; }
 
-    public required List<string> Tags { get; init; }
+    public static Build? FromDomain(Domain.Build.Build item)
+    {
+        if (item is Domain.Build.V1.Build v1)
+        {
+            return new Build()
+            {
+                Branch = v1.Branch,
+                Id = v1.Id,
+                Name = v1.Name,
+                Platform = v1.Platform,
+                Tags = v1.Tags,
+                Target = v1.Target,
+                Crashes = [.. BuildCrash.FromDomain(v1)],
+                Contributions = [.. BuildContribution.FromDomain(v1)],
+                Status = BuildStatus.FromDomain(v1),
+            };
+        }
+
+        throw new NotImplementedException();
+    }
 }
