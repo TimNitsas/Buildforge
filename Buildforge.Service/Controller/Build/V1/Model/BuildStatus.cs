c85cbb4918a7ff3d17e8910f4c2d85acad470f9d
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:25:01 -0400

    Update BuildStatus.cs

--- Buildforge.Service/Controller/Build/V1/Model/BuildStatus.cs
+++ Buildforge.Service/Controller/Build/V1/Model/BuildStatus.cs
@@ -10,4 +10,16 @@ namespace Buildforge.Service.Controller.Build.V1.Model;
 public abstract class BuildStatus
 {
     public DateTime StartTime { get; set; }
+
+    public static BuildStatus FromDomain(Domain.Build.V1.Build item)
+    {
+        return item.Status switch
+        {
+            Domain.Build.V1.BuildStatusActive a => new V1.Model.BuildStatusActive(),
+            Domain.Build.V1.BuildStatusFailed f => new V1.Model.BuildStatusFailed() { Reason = f.Reason },
+            Domain.Build.V1.BuildStatusQueued q => new V1.Model.BuildStatusQueued(),
+            Domain.Build.V1.BuildStatusSuccess s => new V1.Model.BuildStatusSuccess() { BuildTime = s.BuildTime, Bytes = s.Bytes },
+            _ => throw new NotImplementedException()
+        };
+    }
 }
