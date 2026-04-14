2cb9c88a681e4b87e6707aee710c766ef34d8f8a
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:41:06 -0400

    Create JobProviderSimulator.cs

---
+++ Buildforge.Service/Provider/Job/Simulation/JobProviderSimulator.cs
@@ -0,0 +1,26 @@
+﻿using Buildforge.Service.Provider.Job;
+
+namespace Buildforge.Service.Provider.Job.Simulation;
+
+public sealed class JobProviderSimulator : IJobProvider
+{
+    private readonly List<JobProviderSimulatorItem> Jobs = [];
+
+    public JobProviderSimulator(int buildCount)
+    {
+        for (int i = 0; i < buildCount; i++)
+        {
+            Jobs.Add(new JobProviderSimulatorItem(i));
+        }
+    }
+
+    public async IAsyncEnumerable<Job> GetJobs([EnumeratorCancellation] CancellationToken ct)
+    {
+        await Task.Yield();
+
+        foreach (var job in Jobs)
+        {
+            yield return job.Resolve();
+        }
+    }
+}
