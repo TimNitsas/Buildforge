a4e64bcc5e45bec6100e8c8e5f1bd0fc0d6dda28
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 21:10:20 -0400

    Create ContributionProvider.cs

---
+++ Buildforge.Service/Provider/Contribution/ContributionProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace Buildforge.Service.Provider.Contribution;
+
+public interface IContributionProvider
+{
+    IAsyncEnumerable<Contribution> GetContributions(CancellationToken ct);
+}
+
+public sealed class ContributionProvider : IContributionProvider
+{
+    public IAsyncEnumerable<Contribution> GetContributions(CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+}
