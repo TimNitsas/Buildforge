9bf1594ff4b86e3dc1c35e2b560072609bfd3eaa
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 21:04:53 -0400

    Create EventSubscription.cs

---
+++ Buildforge.Service/Domain/Event/EventSubscription.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Channels;
+
+namespace Buildforge.Service.Domain.Event;
+
+public sealed class EventSubscription(EventPublisher owner, ChannelReader<Model.BaseEvent> channelReader) : IAsyncDisposable
+{
+    public ChannelReader<Model.BaseEvent> ChannelReader { get; } = channelReader;
+
+    public async ValueTask DisposeAsync()
+    {
+        await owner.Unsubscribe(this);
+    }
+}
