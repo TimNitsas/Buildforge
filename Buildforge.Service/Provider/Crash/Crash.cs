07e5dcb5dc303fffd0d310cc3fca28e1a098671b
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 21:05:42 -0400

    Create Crash.cs

---
+++ Buildforge.Service/Provider/Crash/Crash.cs
@@ -0,0 +1,14 @@
+﻿namespace Buildforge.Service.Provider.Crash;
+
+public sealed class Crash
+{
+    public required string CrashId { get; set; }
+
+    public required string User { get; init; }
+
+    public required DateTime Date { get; init; }
+
+    public required TimeSpan PlayTime { get; init; }
+
+    public required string BuildId { get; init; }
+}
