fdf9a19a277d459994306ddb42364aef9474a4d9
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:29:09 -0400

    Update BuildItemViewModel.cs

--- Buildforge.App/ViewModel/Build/BuildItemViewModel.cs
+++ Buildforge.App/ViewModel/Build/BuildItemViewModel.cs
@@ -1,15 +1,16 @@
 ﻿using Buildforge.App.Core.Command.Download;
 using Buildforge.Client.V1;
+using CommunityToolkit.Mvvm.Messaging;
 
 namespace Buildforge.App.ViewModel.Build;
 
-public partial class BuildItemViewModel : ObservableObject
+public partial class BuildItemViewModel : ObservableObject, IRecipient<Event.BuildChangedEvent>, IRecipient<Event.BuildCrashedEvent>
 {
     [ObservableProperty]
     private string? name;
 
     [ObservableProperty]
-    private string id;
+    private string? id;
 
     [ObservableProperty]
     private BuildItemStatusViewModel? status;
@@ -23,14 +24,34 @@ public partial class BuildItemViewModel : ObservableObject
     [ObservableProperty]
     private string? platform;
 
+    [ObservableProperty]
+    private int crashCount;
+
     public ObservableCollection<BuildItemContributionViewModel> Contributions { get; }
 
     public ICommand DownloadCommand { get; }
 
     public BuildItemViewModel(Client.V1.Build build)
     {
+        WeakReferenceMessenger.Default.RegisterAll(this);
+
+        Contributions = new(build.Contributions.Select(c => new BuildItemContributionViewModel(c)));
+
         Name = build.Name;
 
+        Target = build.Target;
+
+        Platform = build.Platform;
+
+        Id = build.Id;
+
+        Patch(build);
+
+        DownloadCommand = new DownloadBuildCommand(build.Id);
+    }
+
+    private void Patch(Client.V1.Build build)
+    {
         Status = build.Status switch
         {
             BuildStatusSuccess s => new BuildItemStatusSuccessViewModel(s),
@@ -45,15 +66,25 @@ public partial class BuildItemViewModel : ObservableObject
             BuildStatusSuccess s => s.Bytes,
             _ => null
         };
+    }
 
-        Contributions = new(build.Contributions.Select(c => new BuildItemContributionViewModel(c)));
+    public void Receive(Event.BuildCrashedEvent message)
+    {
+        if (!message.Inner.BuildId.Equals(Id))
+        {
+            return;
+        }
 
-        Target = build.Target;
+        CrashCount++;
+    }
 
-        Platform = build.Platform;
+    public void Receive(Event.BuildChangedEvent message)
+    {
+        if (!message.Inner.Id.Equals(Id))
+        {
+            return;
+        }
 
-        Id = build.Id;
-
-        DownloadCommand = new DownloadBuildCommand(Id);
+        Patch(message.Inner);
     }
 }
