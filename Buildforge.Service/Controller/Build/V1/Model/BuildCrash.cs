817004f0cb819f232932a31e13002c28a3515c56
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:24:51 -0400

    Update BuildCrash.cs

--- Buildforge.Service/Controller/Build/V1/Model/BuildCrash.cs
+++ Buildforge.Service/Controller/Build/V1/Model/BuildCrash.cs
@@ -2,9 +2,22 @@
 
 public sealed class BuildCrash
 {
+    public required string BuildId { get; init; }
+
     public required string User { get; init; }
 
     public required DateTime Date { get; init; }
 
     public required TimeSpan PlayTime { get; init; }
+
+    public static IEnumerable<BuildCrash> FromDomain(Domain.Build.V1.Build item)
+    {
+        return item.BuildCrashes.Select(c => new BuildCrash()
+        {
+            Date = c.Date,
+            PlayTime = c.PlayTime,
+            User = c.User,
+            BuildId = item.Id,
+        });
+    }
 }
