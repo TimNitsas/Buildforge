d8f4436454997f3e7ef6550263739f5374fdd0be
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:32:11 -0400

    Create BuildChangedEvent.cs

---
+++ Buildforge.Service/Controller/Event/V1/Model/BuildChangedEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace Buildforge.Service.Controller.Event.V1.Model;
+
+public sealed class BuildChangedEvent : Event
+{
+    public required Controller.Build.V1.Model.Build Data { get; set; }
+
+    public static BuildChangedEvent? FromDomain(Domain.Event.Model.BuildChangedEvent e)
+    {
+        return new BuildChangedEvent()
+        {
+            Data = Controller.Build.V1.Model.Build.FromDomain(e.Build)
+        };
+    }
+}
