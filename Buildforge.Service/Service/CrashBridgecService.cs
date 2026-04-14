487a13ac0d1d42181d018f5420ca5c73af717432
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:46:53 -0400

    Create CrashBridgecService.cs

---
+++ Buildforge.Service/Service/CrashBridgecService.cs
@@ -0,0 +1,23 @@
+﻿using Buildforge.Service.Domain.Event.Model;
+using Buildforge.Service.Provider.Crash;
+
+namespace Buildforge.Service.Service;
+
+public sealed class CrashBridgeService(ICrashProvider crashProvider, EventPublisher eventPublisher) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken ct)
+    {
+        while (!ct.IsCancellationRequested)
+        {
+            foreach (var crash in crashProvider.GetCrashes(ct))
+            {
+                await eventPublisher.Publish(new BuildCrashedEvent()
+                {
+                    Data = crash
+                });
+            }
+
+            await Task.Delay(1_000, ct);
+        }
+    }
+}
