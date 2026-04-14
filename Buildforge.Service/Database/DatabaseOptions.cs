1cc1fe581bf151f7cdf56237515b240b702cf0d2
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:19:03 -0400

    Create DatabaseOptions.cs

---
+++ Buildforge.Service/Database/DatabaseOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace Buildforge.Service.Database;
+
+public sealed class DatabaseOptions
+{
+    public required string Host { get; init; }
+
+    public required int Port { get; init; }
+
+    public required string Database { get; init; }
+
+    public required string Username { get; init; }
+
+    public required string Password { get; init; }
+
+    public string ToConnectionString()
+    {
+        return $"Host={Host};Port={Port};Database={Database};Username={Username};Password={Password};GSS Encryption Mode=disable";
+    }
+}
