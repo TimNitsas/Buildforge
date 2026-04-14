dd60aae361b4ee406e289801a6365f91ec2ae2c4
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:28:55 -0400

    Create Client.Extension.cs

---
+++ Buildforge.Client/V1/Client.Extension.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.ServerSentEvents;
+using System.Runtime.CompilerServices;
+
+namespace Buildforge.Client.V1;
+
+public partial interface IEventClient
+{
+    IAsyncEnumerable<Event> SubscribeSseImpl(CancellationToken ct);
+}
+
+public partial class EventClient : IEventClient
+{
+    public async IAsyncEnumerable<Event> SubscribeSseImpl([EnumeratorCancellation] CancellationToken ct)
+    {
+        using var stream = await _httpClient.GetStreamAsync($"{_baseUrl}api/v1/events/subscribeSse", ct);
+
+        await foreach (SseItem<string> item in SseParser.Create(stream).EnumerateAsync(ct))
+        {
+            var e = Newtonsoft.Json.JsonConvert.DeserializeObject<Event>(item.Data, JsonSerializerSettings);
+
+            if (e is not null)
+            {
+                yield return e;
+            }
+        }
+    }
+}
