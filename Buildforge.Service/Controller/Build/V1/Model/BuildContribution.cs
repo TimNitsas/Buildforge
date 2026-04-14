2a75acbfc113d16cfcbe290750626d55218de85a
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-13 20:24:47 -0400

    Update BuildContribution.cs

--- Buildforge.Service/Controller/Build/V1/Model/BuildContribution.cs
+++ Buildforge.Service/Controller/Build/V1/Model/BuildContribution.cs
@@ -7,4 +7,17 @@ public sealed class BuildContribution
     public required string User { get; init; }
 
     public required string Description { get; init; }
+
+    public static IEnumerable<BuildContribution> FromDomain(Domain.Build.V1.Build item)
+    {
+        foreach (var contribution in item.Contributions)
+        {
+            yield return new BuildContribution()
+            {
+                Description = contribution.Description,
+                Id = contribution.Id,
+                User = contribution.Id
+            };
+        }
+    }
 }
