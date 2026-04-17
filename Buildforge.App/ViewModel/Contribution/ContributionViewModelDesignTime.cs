93822f097765f8754db4b7bbdd724378e909ca33
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-16 21:26:57 -0400

    Update ContributionViewModelDesignTime.cs

--- Buildforge.App/ViewModel/Contribution/ContributionViewModelDesignTime.cs
+++ Buildforge.App/ViewModel/Contribution/ContributionViewModelDesignTime.cs
@@ -1,5 +1,15 @@
-﻿namespace Buildforge.App.ViewModel.Contribution;
+﻿using Bogus;
+using Buildforge.Client.V1;
+
+namespace Buildforge.App.ViewModel.Contribution;
 
 public sealed class ContributionViewModelDesignTime : ContributionViewModel
 {
+    public ContributionViewModelDesignTime() : base(new MockContributionClient())
+    {
+        foreach (var contribution in MockContributionClient.GetMockData().Contributions)
+        {
+            Contributions.Add(new ContributionItemViewModel(contribution));
+        }
+    }
 }
