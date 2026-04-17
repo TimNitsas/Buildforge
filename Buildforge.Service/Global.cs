b263eb76a1969e657b4ee8997cac59eaa0afbb95
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-16 21:27:45 -0400

    Update Global.cs

--- Buildforge.Service/Global.cs
+++ Buildforge.Service/Global.cs
@@ -7,5 +7,4 @@ global using System.Net.Http.Headers;
 global using System.Runtime.CompilerServices;
 global using System.Text;
 global using System.Text.Json;
-global using Buildforge.Service.Domain.Build;
 global using Buildforge.Service.Domain.Event;
