e08d70a35c3a61bb438e58e8398071cbf291d276
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-16 21:35:15 -0400

    Create ContributionBridgeService.cs

---
+++ Buildforge.Service/Service/ContributionBridgeService.cs
@@ -0,0 +1,11 @@
+﻿using Buildforge.Service.Provider.Contribution;
+
+namespace Buildforge.Service.Service;
+
+public sealed class ContributionBridgeService(IContributionProvider contributionProvider) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken ct)
+    {
+        var data = await contributionProvider.GetContributions(ct).ToListAsync();
+    }
+}
