aaae2a679b8bf5bf33b17067fc92892cf1f06283
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-17 19:30:35 -0400

    Create HumanizeTimeSpanConverter.cs

---
+++ Buildforge.App/Converter/HumanizeTimeSpanConverter.cs
@@ -0,0 +1,21 @@
+﻿using Humanizer;
+
+namespace Buildforge.App.Converter;
+
+public sealed class HumanizeTimeSpanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is TimeSpan ts)
+        {
+            return ts.Humanize();
+        }
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
