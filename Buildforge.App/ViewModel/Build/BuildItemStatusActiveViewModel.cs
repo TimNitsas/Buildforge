cbe30ce06a9184586d0e8160e20fe64113f124da
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-16 21:33:22 -0400

    Update BuildItemStatusActiveViewModel.cs

--- Buildforge.App/ViewModel/Build/BuildItemStatusActiveViewModel.cs
+++ Buildforge.App/ViewModel/Build/BuildItemStatusActiveViewModel.cs
@@ -1,8 +1,38 @@
-﻿namespace Buildforge.App.ViewModel.Build;
+﻿using Buildforge.App.Messaging;
+using Humanizer;
+
+namespace Buildforge.App.ViewModel.Build;
 
 public partial class BuildItemStatusActiveViewModel : BuildItemStatusViewModel
 {
+    [ObservableProperty]
+    private double completionValue;
+
+    [ObservableProperty]
+    private string? completionText;
+
+    private readonly Client.V1.BuildStatusActive Status;
+
     public BuildItemStatusActiveViewModel(Client.V1.BuildStatusActive status) : base(status)
     {
+        Status = status;
+
+        ComputeProgress(status);
+    }
+
+    private void ComputeProgress(Client.V1.BuildStatusActive status)
+    {
+        var duration = status.EstimatedTimeToCompletion - status.StartTime;
+
+        var elapsed = DateTime.UtcNow - status.StartTime;
+
+        CompletionValue = 100 * elapsed.TotalSeconds / duration.TotalSeconds;
+
+        CompletionText = $"{(elapsed - duration).Humanize()} left";
+    }
+
+    protected override void OnReceive(TickTimeRefreshEvent message)
+    {
+        ComputeProgress(Status);
     }
 }
