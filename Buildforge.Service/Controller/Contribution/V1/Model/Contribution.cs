86139c2985039ed128b887bf32ee7c0fc413c317
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-16 21:27:40 -0400

    Create Contribution.cs

---
+++ Buildforge.Service/Controller/Contribution/V1/Model/Contribution.cs
@@ -0,0 +1,37 @@
+﻿namespace Buildforge.Service.Controller.Contribution.V1.Model;
+
+public sealed class Contribution
+{
+    public required string Id { get; init; }
+
+    public required DateTime CommitDate { get; init; }
+
+    public required string User { get; init; }
+
+    public required string Description { get; init; }
+
+    public required List<ContributionFile> Files { get; set; }
+
+    public static Contribution FromDomain(Repository.Contribution.Contribution c)
+    {
+        if (c is Repository.Contribution.V1.Contribution v1)
+        {
+            var files = v1.Files.Select(f => new V1.Model.ContributionFile
+            {
+                Path = f.Path,
+                Size = f.Size
+            });
+
+            return new Contribution()
+            {
+                Description = v1.Description,
+                Id = v1.Id,
+                User = v1.User,
+                CommitDate = v1.CommitDate,
+                Files = files.ToList()
+            };
+        }
+
+        throw new NotImplementedException();
+    }
+}
