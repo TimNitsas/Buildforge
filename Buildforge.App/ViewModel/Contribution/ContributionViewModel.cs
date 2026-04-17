f33ed611906f8a54f234c9e2014bd31fbe866d83
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-16 21:26:08 -0400

    Update ContributionViewModel.cs

--- Buildforge.App/ViewModel/Contribution/ContributionViewModel.cs
+++ Buildforge.App/ViewModel/Contribution/ContributionViewModel.cs
@@ -1,5 +1,24 @@
-﻿namespace Buildforge.App.ViewModel.Contribution;
+﻿using Buildforge.Client.V1;
 
-public partial class ContributionViewModel : ObservableObject
+namespace Buildforge.App.ViewModel.Contribution;
+
+public partial class ContributionViewModel(IContributionClient client) : ObservableObject
 {
+    public ObservableCollection<ContributionItemViewModel> Contributions { get; } = [];
+
+    [RelayCommand]
+    public async Task Load()
+    {
+        var result = await client.GetContributionsAsync(0);
+
+        if (result is null)
+        {
+            return;
+        }
+
+        foreach (var item in result.Contributions)
+        {
+            Contributions.Add(new ContributionItemViewModel(item));
+        }
+    }
 }
