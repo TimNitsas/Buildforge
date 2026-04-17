555ffab92a7a05fdceda0ce581080582109e3d3c
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-16 21:30:08 -0400

    Create ContributionFile.cs

---
+++ Buildforge.Service/Repository/Contribution/V1/ContributionFile.cs
@@ -0,0 +1,8 @@
+﻿namespace Buildforge.Service.Repository.Contribution.V1;
+
+public sealed class ContributionFile
+{
+    public required string Path { get; set; }
+
+    public required byte Size { get; set; }
+}
