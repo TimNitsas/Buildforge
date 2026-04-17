c195d53574012d6a76e02f1a7c5a334ccbbc5ceb
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-16 21:32:39 -0400

    Create ContributionItemViewModel.cs

---
+++ Buildforge.App/ViewModel/Contribution/ContributionItemViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Humanizer;
+
+namespace Buildforge.App.ViewModel.Contribution;
+
+public sealed partial class ContributionItemViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string id;
+
+    [ObservableProperty]
+    private string user;
+
+    [ObservableProperty]
+    private string description;
+
+    [ObservableProperty]
+    private DateTime commitDate;
+
+    [ObservableProperty]
+    private int fileCount;
+
+    [ObservableProperty]
+    private int fileSize;
+
+    public ContributionItemViewModel(Client.V1.Contribution item)
+    {
+        User = item.User;
+
+        Description = item.Description;
+
+        Id = item.Id;
+
+        CommitDate = item.CommitDate.Date;
+
+        FileCount = item.Files.Count;
+
+        FileSize = item.Files.Sum(f => f.Size);
+    }
+}
