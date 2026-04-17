2510e3ef179c54bd3441c75a9022f4cec4c8be89
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-16 21:24:04 -0400

    Move

---
+++ Buildforge.App/Event/TickTimeRefreshEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace Buildforge.App.Messaging;
+
+public class TickTimeRefreshEvent
+{
+    public static readonly TickTimeRefreshEvent Instance = new();
+}
--- Buildforge.App/Messaging/TickTimeMessage.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Buildforge.App.Messaging;
-
-public class TickTimeMessage
-{
-    public static readonly TickTimeMessage Instance = new();
-}
