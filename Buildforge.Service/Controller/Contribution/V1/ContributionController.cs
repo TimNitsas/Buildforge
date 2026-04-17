7c794087b2946ede9ba98414966cdbc332ece0ae
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-16 21:27:15 -0400

    Create ContributionController.cs

---
+++ Buildforge.Service/Controller/Contribution/V1/ContributionController.cs
@@ -0,0 +1,31 @@
+﻿using Buildforge.Service.Controller.Contribution.V1.Model;
+using Buildforge.Service.Repository.Contribution;
+
+namespace Buildforge.Service.Controller.Contribution.V1;
+
+[ApiController]
+[Route("api/v1/contributions")]
+[ApiExplorerSettings(GroupName = "v1")]
+public class ContributionController(ContributionRepository contributionRepository) : ControllerBase
+{
+    public class GetContributionsQueryParameters
+    {
+        public int Skip { get; set; }
+    }
+
+    [HttpGet]
+    public async Task<ContributionResult> GetContributions([FromQuery] GetContributionsQueryParameters query, CancellationToken ct)
+    {
+        var result = new ContributionResult()
+        {
+            Contributions = []
+        };
+
+        foreach (var contribution in await contributionRepository.GetContributions(ct).Skip(query.Skip).ToListAsync(cancellationToken: ct))
+        {
+            result.Contributions.Add(Model.Contribution.FromDomain(contribution));
+        }
+
+        return result;
+    }
+}
