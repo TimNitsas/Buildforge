659ce2e98c34852037f3de475dc9d71605a5f10c
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-16 21:34:46 -0400

    Update ContributionView.xaml.cs

--- Buildforge.App/View/ContributionView.xaml.cs
+++ Buildforge.App/View/ContributionView.xaml.cs
@@ -1,9 +1,13 @@
-﻿namespace Buildforge.App.View;
+﻿using Buildforge.App.ViewModel.Contribution;
+
+namespace Buildforge.App.View;
 
 public partial class ContributionView : UserControl
 {
     public ContributionView()
     {
         InitializeComponent();
+
+        DataContext = App.Services.GetRequiredService<ContributionViewModel>();
     }
 }
