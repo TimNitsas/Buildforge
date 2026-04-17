fe2bcc96f2b9f2e1ea0c63541ac0b8a617f2f784
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-17 19:30:38 -0400

    Create NullVisibilityConverter.cs

---
+++ Buildforge.App/Converter/NullVisibilityConverter.cs
@@ -0,0 +1,14 @@
+﻿namespace Buildforge.App.Converter;
+
+public class NullVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is null ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
