f7e719375c2b5634493fe8c82f47423bbff6c83b
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-16 21:30:12 -0400

    Create Contribution.cs

---
+++ Buildforge.Service/Repository/Contribution/V1/Contribution.cs
@@ -0,0 +1,12 @@
+﻿namespace Buildforge.Service.Repository.Contribution.V1;
+
+public sealed class Contribution : Repository.Contribution.Contribution
+{
+    public required string User { get; init; }
+
+    public required string Description { get; init; }
+
+    public required DateTime CommitDate { get; set; }
+
+    public required List<ContributionFile> Files { get; set; }
+}
