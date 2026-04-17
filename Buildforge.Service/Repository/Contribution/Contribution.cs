d06471f51eac7984429c1879e473b456377557e9
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-16 21:30:50 -0400

    Create Contribution.cs

---
+++ Buildforge.Service/Repository/Contribution/Contribution.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Buildforge.Service.Repository.Contribution;
+
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "Version")]
+[JsonDerivedType(typeof(V1.Contribution), "v1")]
+public abstract class Contribution
+{
+    public required string Id { get; init; }
+
+    public required DateTime ReadAt { get; set; }
+}
