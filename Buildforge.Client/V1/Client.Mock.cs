7ae236fdf32b7148f4892cd008e4079904bc9f18
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-16 21:32:29 -0400

    Update Client.Mock.cs

--- Buildforge.Client/V1/Client.Mock.cs
+++ Buildforge.Client/V1/Client.Mock.cs
@@ -136,3 +136,60 @@ public partial class MockAuthenticationClient : IAuthenticationClient
         });
     }
 }
+
+public partial class MockEventClient : IEventClient
+{
+    public Task<ICollection<Event>> SubscribeAsync(CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult<ICollection<Event>>([]);
+    }
+
+    public Task SubscribeSseAsync(CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    public async IAsyncEnumerable<Event> SubscribeSseImpl([EnumeratorCancellation] CancellationToken ct)
+    {
+        await Task.Yield();
+
+        yield break;
+    }
+}
+
+public partial class MockContributionClient : IContributionClient
+{
+    public Task<ContributionResult> GetContributionsAsync(int? skip = null, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(GetMockData());
+    }
+
+    public static ContributionResult GetMockData()
+    {
+        var result = new ContributionResult();
+
+        var faker = new Faker();
+
+        var random = new Random(42);
+
+        for (int i = 0; i < 100; i++)
+        {
+            var files = Enumerable.Range(1, random.Next(1, 20)).Select(i => new ContributionFile()
+            {
+                Path = "some_path",
+                Size = i * 1024
+            });
+
+            result.Contributions.Add(new Client.V1.Contribution()
+            {
+                User = NameGenerator.PersonNames.Get(),
+                Id = (i * i + i).ToString(),
+                Description = faker.Lorem.Sentences(3),
+                CommitDate = DateTime.Now.AddMinutes(-random.Next(1, 60 * 4)),
+                Files = [.. files]
+            });
+        }
+
+        return result;
+    }
+}
