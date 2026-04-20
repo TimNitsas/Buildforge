5827e01e85e964c5c14c6681f12fd1fef93c217c
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-19 20:35:04 -0400

    Create ContributionRecord.cs

---
+++ Buildforge.Service/Repository/Contribution/ContributionRecord.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Buildforge.Service.Repository.Contribution;
+
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "Version")]
+[JsonDerivedType(typeof(V1.ContributionRecord), "v1")]
+public abstract class ContributionRecord
+{
+}
