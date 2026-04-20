830be292095c7ae7a3ea7d77f9d768a5046706c4
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-19 20:35:30 -0400

    Update ContributionBridgeService.cs

--- Buildforge.Service/Service/ContributionBridgeService.cs
+++ Buildforge.Service/Service/ContributionBridgeService.cs
@@ -5,6 +5,13 @@ namespace Buildforge.Service.Service;
 
 public sealed class ContributionBridgeService(IContributionProvider contributionProvider, ContributionRepository contributionRepository) : BackgroundService
 {
+    public override async Task StartAsync(CancellationToken ct)
+    {
+        await contributionRepository.Initialize(ct);
+
+        await base.StartAsync(ct);
+    }
+
     protected override async Task ExecuteAsync(CancellationToken ct)
     {
         var data = await contributionProvider.GetContributions(ct).ToListAsync();
