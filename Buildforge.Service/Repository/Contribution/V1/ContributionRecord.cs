745264410514f01b316d69d90d5a79787015dee1
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-19 20:35:01 -0400

    Create ContributionRecord.cs

---
+++ Buildforge.Service/Repository/Contribution/V1/ContributionRecord.cs
@@ -0,0 +1,6 @@
+﻿namespace Buildforge.Service.Repository.Contribution.V1;
+
+public sealed class ContributionRecord : Repository.Contribution.ContributionRecord
+{
+    public required string LastKnownChange { get; set; }
+}
