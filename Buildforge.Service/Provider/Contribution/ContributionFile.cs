3ebf068ea1aab85570ff3a0410bed2836c177654
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-04-19 20:32:59 -0400

    Create ContributionFile.cs

---
+++ Buildforge.Service/Provider/Contribution/ContributionFile.cs
@@ -0,0 +1,12 @@
+﻿namespace Buildforge.Service.Provider.Contribution;
+
+public sealed class ContributionFile
+{
+    public required string DepotPath { get; init; }
+
+    public required long? Size { get; init; }
+
+    public required ContributionAction? ContributionAction { get; init; }
+
+    public required string Hash { get; init; }
+}
