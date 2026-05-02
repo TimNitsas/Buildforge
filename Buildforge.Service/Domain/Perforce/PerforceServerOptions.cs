dd71065cf9b815e4e2f5a5ffa0dd41fcc2f3a3d0
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-01 22:01:05 -0400

    Create PerforceServerOptions.cs

---
+++ Buildforge.Service/Domain/Perforce/PerforceServerOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Buildforge.Service.Domain.Perforce;
+
+public sealed class PerforceServerOptions
+{
+    public required string Host { get; set; }
+
+    public required string Username { get; set; }
+}
