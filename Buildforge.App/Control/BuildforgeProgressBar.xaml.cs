fc602f2229766546031a471a1ca6463fe393a958
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-01 22:03:01 -0400

    Create BuildforgeProgressBar.xaml.cs

---
+++ Buildforge.App/Control/BuildforgeProgressBar.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Buildforge.App.Control;
+
+public partial class BuildforgeProgressBar : UserControl
+{
+    public string ProgressText
+    {
+        get { return (string)GetValue(ProgressTextProperty); }
+        set { SetValue(ProgressTextProperty, value); }
+    }
+
+    public static readonly DependencyProperty ProgressTextProperty = DependencyProperty.Register(nameof(ProgressText), typeof(string), typeof(BuildforgeProgressBar), new PropertyMetadata(string.Empty));
+
+    public double ProgressValue
+    {
+        get { return (double)GetValue(ProgressValueProperty); }
+        set { SetValue(ProgressValueProperty, value); }
+    }
+
+    public static readonly DependencyProperty ProgressValueProperty = DependencyProperty.Register(nameof(ProgressValue), typeof(double), typeof(BuildforgeProgressBar), new PropertyMetadata(0d));
+
+    public BuildforgeProgressBar()
+    {
+        InitializeComponent();
+    }
+}
