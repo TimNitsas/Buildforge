8dee27fd330f0ef7f85e3256f77483e9f4db05dc
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-01 21:58:58 -0400

    Create ContributionBuild.cs

---
+++ Buildforge.Service/Controller/Contribution/V1/Model/ContributionBuild.cs
@@ -0,0 +1,10 @@
+﻿namespace Buildforge.Service.Controller.Contribution.V1.Model;
+
+public sealed class ContributionBuild
+{
+    public required string Id { get; set; }
+
+    public required string Status { get; set; }
+
+    public required string Url { get; set; }
+}
