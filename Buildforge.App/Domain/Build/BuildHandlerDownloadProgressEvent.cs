f5b7f7e2b4e64a78f8c8003966412f0fb51e0b67
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-01 22:03:21 -0400

    Create BuildHandlerDownloadProgressEvent.cs

---
+++ Buildforge.App/Domain/Build/BuildHandlerDownloadProgressEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace Buildforge.App.Domain.Build;
+
+public sealed class BuildHandlerDownloadProgressEvent
+{
+    public required string BuildId { get; init; }
+
+    public required long BytesReceived { get; init; }
+
+    public required long TotalBytes { get; init; }
+
+    public required double ProgressPercentage { get; init; }
+
+    public required double SpeedKbps { get; init; }
+
+    public TimeSpan EstimatedEta { get; init; }
+}
