b4d22e15a389f209bddc777ed64c09f129cdd73e
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-01 21:59:19 -0400

    Update Event.cs

--- Buildforge.Service/Controller/Event/V1/Model/Event.cs
+++ Buildforge.Service/Controller/Event/V1/Model/Event.cs
@@ -1,11 +1,13 @@
-﻿using System.Text.Json.Serialization;
+﻿using Buildforge.Service.Domain.Event.Model;
+using System.Text.Json.Serialization;
 
 namespace Buildforge.Service.Controller.Event.V1.Model;
 
 [JsonPolymorphic(TypeDiscriminatorPropertyName = "discriminator")]
-[JsonDerivedType(typeof(BuildCreatedEvent), typeDiscriminator: nameof(BuildCreatedEvent))]
-[JsonDerivedType(typeof(BuildCrashEvent), typeDiscriminator: nameof(BuildCrashEvent))]
-[JsonDerivedType(typeof(BuildChangedEvent), typeDiscriminator: nameof(BuildChangedEvent))]
+[JsonDerivedType(typeof(BuildCreatedEvent), typeDiscriminator: "build_created")] // no nameof() for stability
+[JsonDerivedType(typeof(BuildCrashEvent), typeDiscriminator: "build_crashed")] // no nameof() for stability
+[JsonDerivedType(typeof(BuildChangedEvent), typeDiscriminator: "build_changed")] // no nameof() for stability
+[JsonDerivedType(typeof(ContributionCreatedEvent), typeDiscriminator: "contribution_created")] // no nameof() for stability
 public abstract class Event
 {
     public static Event FromDomain(Domain.Event.Model.BaseEvent e)
@@ -15,6 +17,7 @@ public abstract class Event
             Domain.Event.Model.BuildCrashedEvent buildCrashEvent => BuildCrashEvent.FromDomain(buildCrashEvent),
             Domain.Event.Model.BuildCreatedEvent buildCreatedEvent => BuildCreatedEvent.FromDomain(buildCreatedEvent),
             Domain.Event.Model.BuildChangedEvent buildChangedEvent => BuildChangedEvent.FromDomain(buildChangedEvent),
+            Domain.Event.Model.ContributionCreatedEvent contributionCreatedEvent => ContributionCreatedEvent.FromDomain(contributionCreatedEvent),
             _ => throw new NotImplementedException(e.GetType().ToString())
         };
     }
