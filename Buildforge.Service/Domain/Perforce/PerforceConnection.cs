bd0f72c6984d083bcb333a32114223f491d567c6
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-01 22:04:09 -0400

    Create PerforceConnection.cs

---
+++ Buildforge.Service/Domain/Perforce/PerforceConnection.cs
@@ -0,0 +1,26 @@
+﻿using Perforce.P4;
+
+namespace Buildforge.Service.Domain.Perforce;
+
+public class PerforceConnection : IDisposable
+{
+    public global::Perforce.P4.Connection Connection { get; }
+
+    public PerforceConnection(IOptions<PerforceServerOptions> options)
+    {
+        Server pServer = new global::Perforce.P4.Server(new ServerAddress(options.Value.Host));
+
+        global::Perforce.P4.Repository rep = new(pServer);
+
+        Connection = rep.Connection;
+
+        Connection.UserName = options.Value.Username;
+
+        Connection.Connect(null);
+    }
+
+    public void Dispose()
+    {
+        Connection.Dispose();
+    }
+}
