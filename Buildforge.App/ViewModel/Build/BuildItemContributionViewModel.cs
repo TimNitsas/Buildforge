6821141fdec57d56f334a132527acca268dd4500
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-01 22:04:40 -0400

    Update BuildItemContributionViewModel.cs

--- Buildforge.App/ViewModel/Build/BuildItemContributionViewModel.cs
+++ Buildforge.App/ViewModel/Build/BuildItemContributionViewModel.cs
@@ -5,8 +5,13 @@ public partial class BuildItemContributionViewModel : ObservableObject
     [ObservableProperty]
     private string? user;
 
+    [ObservableProperty]
+    private string? id;
+
     public BuildItemContributionViewModel(Client.V1.BuildContribution buildContribution)
     {
         User = buildContribution.User;
+
+        Id = buildContribution.Id;
     }
 }
