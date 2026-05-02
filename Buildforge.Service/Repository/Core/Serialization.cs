b9f7954f5108b0b48d747c667cf7f8ead3a417a7
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-01 22:00:05 -0400

    Create Serialization.cs

---
+++ Buildforge.Service/Repository/Core/Serialization.cs
@@ -0,0 +1,9 @@
+﻿namespace Buildforge.Service.Repository.Core;
+
+public class Serialization
+{
+    public static readonly JsonSerializerOptions JsonSerializerOptions = new()
+    {
+        AllowOutOfOrderMetadataProperties = true
+    };
+}
