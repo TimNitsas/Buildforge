beacc827b7b7bff790285b5951a5c2b9bd8500ea
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-01 22:00:42 -0400

    Update ContributionRepository.cs

--- Buildforge.Service/Repository/Contribution/ContributionRepository.cs
+++ Buildforge.Service/Repository/Contribution/ContributionRepository.cs
@@ -1,11 +1,18 @@
-﻿using Buildforge.Service.Repository.Contribution.V1;
+﻿using Buildforge.Service.Domain.Event.Model;
+using Buildforge.Service.Repository.Contribution.V1;
+using Buildforge.Service.Repository.Core;
 using Npgsql;
+using NpgsqlTypes;
 
 namespace Buildforge.Service.Repository.Contribution;
 
-public class ContributionRepository(Database.Database database)
+public class ContributionRepository(Database.Database database, EventPublisher eventPublisher)
 {
-    internal async IAsyncEnumerable<Contribution> GetContributions([EnumeratorCancellation] CancellationToken ct)
+    private readonly string TableName = "contributions";
+
+    private readonly SemaphoreSlim Semaphore = new(1, 1);
+
+    public async IAsyncEnumerable<Contribution> GetContributions([EnumeratorCancellation] CancellationToken ct)
     {
         await Task.Yield();
 
@@ -18,8 +25,71 @@ public class ContributionRepository(Database.Database database)
                 Files = new List<ContributionFile>(),
                 Id = i.ToString(),
                 User = i.ToString(),
-                CommitDate = DateTime.UtcNow
+                CommitDate = DateTime.UtcNow,
+                Builds = new List<ContributionBuild>(),
             };
         }
+    }
+
+    public async Task Initialize(CancellationToken ct)
+    {
+        try
+        {
+            await Semaphore.WaitAsync(ct);
+
+            using var connection = await database.DataSource.OpenConnectionAsync(ct);
+
+            string sql = @$"
+                CREATE TABLE IF NOT EXISTS {TableName} (
+                id TEXT PRIMARY KEY,
+                data JSONB NOT NULL,
+                read_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+            );";
+
+            await using var cmd = new NpgsqlCommand(sql, connection);
+
+            await cmd.ExecuteNonQueryAsync(ct);
+        }
+        finally
+        {
+            Semaphore.Release();
+        }
+    }
+
+    public async Task SaveContribution(Contribution contribution, CancellationToken ct)
+    {
+        using var connection = await database.DataSource.OpenConnectionAsync(ct);
+
+        var json = JsonSerializer.Serialize(contribution, Serialization.JsonSerializerOptions);
+
+        string sql = $@"
+            INSERT INTO {TableName} (id, data, read_at)
+            VALUES (@id, @data, @read_at)
+            ON CONFLICT (id)
+            DO UPDATE SET data = EXCLUDED.data;
+        ";
+
+        await using var cmd = new NpgsqlCommand(sql, connection);
+
+        cmd.Parameters.AddWithValue("id", contribution.Id);
+        cmd.Parameters.AddWithValue("data", NpgsqlDbType.Jsonb, json);
+        cmd.Parameters.AddWithValue("read_at", contribution.ReadAt);
+
+        await cmd.ExecuteNonQueryAsync(ct);
+
+        await eventPublisher.Publish(new ContributionCreatedEvent()
+        {
+            Contribution = contribution,
+        });
+    }
+
+    public Task WriteLastKnownChange(string lastKnownChange)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task<string?> GetLastKnownChange(CancellationToken ct)
+    {
+        return Task.FromResult<string?>(string.Empty);
     }
 }
