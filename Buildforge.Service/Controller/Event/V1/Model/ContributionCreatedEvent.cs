0033beb8455b9456d10f316c9f982f9bdfb5c997
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-01 22:28:52 -0400

    Create ContributionCreatedEvent.cs

---
+++ Buildforge.Service/Controller/Event/V1/Model/ContributionCreatedEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace Buildforge.Service.Controller.Event.V1.Model;
+
+public sealed class ContributionCreatedEvent : Event
+{
+    public required Contribution.V1.Model.Contribution Data { get; set; }
+
+    public static ContributionCreatedEvent FromDomain(Domain.Event.Model.ContributionCreatedEvent e)
+    {
+        return new ContributionCreatedEvent()
+        {
+            Data = Contribution.V1.Model.Contribution.FromDomain(e.Contribution)
+        };
+    }
+}
