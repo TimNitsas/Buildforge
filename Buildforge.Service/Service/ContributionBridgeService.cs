f2e974edc5b62ced6ae8706c6cdf2b79cb42d276
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-01 22:28:32 -0400

    Update ContributionBridgeService.cs

--- Buildforge.Service/Service/ContributionBridgeService.cs
+++ Buildforge.Service/Service/ContributionBridgeService.cs
@@ -14,6 +14,33 @@ public sealed class ContributionBridgeService(IContributionProvider contribution
 
     protected override async Task ExecuteAsync(CancellationToken ct)
     {
-        var data = await contributionProvider.GetContributions(ct).ToListAsync();
+        var start = await contributionRepository.GetLastKnownChange(ct);
+
+        while (!ct.IsCancellationRequested)
+        {
+            await foreach (var item in contributionProvider.GetContributions(start, ct))
+            {
+                var files = item.Files.Select(f => new Repository.Contribution.V1.ContributionFile()
+                {
+                    Path = f.DepotPath,
+                    Size = f.Size,
+                });
+
+                Repository.Contribution.V1.Contribution contribution = new()
+                {
+                    CommitDate = item.CommitDate,
+                    Description = item.Description,
+                    Id = item.Id,
+                    ReadAt = DateTime.UtcNow,
+                    User = item.User,
+                    Files = [.. files],
+                    Builds = []
+                };
+
+                await contributionRepository.SaveContribution(contribution, ct);
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(10), ct);
+        }
     }
 }
