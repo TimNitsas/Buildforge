41a45c291f3bf4b8c514f4b81c9e4d377b45aa46
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-01 22:28:48 -0400

    Create ContributionCreatedEvent.cs

---
+++ Buildforge.Service/Domain/Event/Model/ContributionCreatedEvent.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Buildforge.Service.Domain.Event.Model;
+
+public sealed class ContributionCreatedEvent : BaseEvent
+{
+    public required Repository.Contribution.Contribution Contribution { get; init; }
+}
