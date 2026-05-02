87d6c44b4d69a6903d57d8cbe2491a3460b0f23d
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-01 22:01:43 -0400

    Create ContributionItemBuildViewModel.cs

---
+++ Buildforge.App/ViewModel/Contribution/ContributionItemBuildViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Buildforge.Client.V1;
+
+namespace Buildforge.App.ViewModel.Contribution;
+
+public sealed partial class ContributionItemBuildViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string id;
+
+    [ObservableProperty]
+    private string url;
+
+    [ObservableProperty]
+    private string status;
+
+    public ContributionItemBuildViewModel(ContributionBuild build)
+    {
+        Id = build.Id;
+
+        Url = build.Url;
+
+        Status = build.Status;
+    }
+}
