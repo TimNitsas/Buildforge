9cb0055d5849287bd7120c51cffe0dee5d329249
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-04 22:54:55 -0400

    Create BuildItemContributionItemViewModelDesignTime.cs

---
+++ Buildforge.App/ViewModel/Build/BuildItemContributionItemViewModelDesignTime.cs
@@ -0,0 +1,24 @@
+﻿using Bogus;
+using Buildforge.Client.V1;
+using RandomFriendlyNameGenerator;
+
+namespace Buildforge.App.ViewModel.Build;
+
+public sealed class BuildItemContributionItemViewModelDesignTime : BuildItemContributionItemViewModel
+{
+    public BuildItemContributionItemViewModelDesignTime() : base(GetBuildContribution())
+    {
+    }
+
+    public static BuildContribution GetBuildContribution()
+    {
+        var faker = new Faker();
+
+        return new BuildContribution()
+        {
+            User = NameGenerator.PersonNames.Get(),
+            Id = new Random().Next(1, 1_000_000).ToString(),
+            Description = faker.Lorem.Sentences(3),
+        };
+    }
+}
