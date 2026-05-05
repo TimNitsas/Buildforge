b00a536e4683f601529ea79618f92ece9ac75373
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-04 22:54:50 -0400

    Create BuildItemContributionItemViewModel.cs

---
+++ Buildforge.App/ViewModel/Build/BuildItemContributionItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Buildforge.App.ViewModel.Build;
+
+public partial class BuildItemContributionItemViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string? user;
+
+    [ObservableProperty]
+    private string? id;
+
+    [ObservableProperty]
+    private string? description;
+
+    public BuildItemContributionItemViewModel(Client.V1.BuildContribution buildContribution)
+    {
+        User = buildContribution.User;
+
+        Id = buildContribution.Id;
+
+        Description = buildContribution.Description.Replace("\n", " ");
+    }
+}
