e343d413fd5a996c859d0b5376e59016d0b57e7a
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-04 22:55:01 -0400

    Create BuildItemContributionViewModelDesignTime.cs

---
+++ Buildforge.App/ViewModel/Build/BuildItemContributionViewModelDesignTime.cs
@@ -0,0 +1,16 @@
+﻿namespace Buildforge.App.ViewModel.Build;
+
+public sealed class BuildItemContributionViewModelDesignTime : BuildItemContributionViewModel
+{
+    public BuildItemContributionViewModelDesignTime() : base(GetContributions())
+    {
+    }
+
+    private static IEnumerable<Client.V1.BuildContribution> GetContributions()
+    {
+        for (int i = 0; i < new Random().Next(1, 10); i++)
+        {
+            yield return BuildItemContributionItemViewModelDesignTime.GetBuildContribution();
+        }
+    }
+}
