cf088b936ee72030e211d25590998d244721e1a4
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-09 16:11:09 -0400

    Create RootViewModelDesignTime.cs

---
+++ Buildforge.App/ViewModel/Root/RootViewModelDesignTime.cs
@@ -0,0 +1,10 @@
+﻿using Buildforge.App.Domain.Token;
+
+namespace Buildforge.App.ViewModel.Root;
+
+public partial class RootViewModelDesignTime : RootViewModel
+{
+    public RootViewModelDesignTime() : base(new TokenHandler())
+    {
+    }
+}
