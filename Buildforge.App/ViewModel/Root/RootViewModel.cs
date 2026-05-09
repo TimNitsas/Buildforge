d2ca68f2ca07302403459169b241be207b935a97
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-09 16:11:06 -0400

    Create RootViewModel.cs

---
+++ Buildforge.App/ViewModel/Root/RootViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Buildforge.App.Domain.Token;
+using Buildforge.App.Event;
+using Buildforge.App.ViewModel.Authentication;
+using Buildforge.App.ViewModel.Main;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace Buildforge.App.ViewModel.Root;
+
+public partial class RootViewModel : ObservableObject, IRecipient<TokenAcquiredEvent>
+{
+    private readonly TokenHandler TokenHandler;
+
+    [ObservableProperty]
+    private object? currentViewModel;
+
+    public RootViewModel(TokenHandler tokenHandler)
+    {
+        WeakReferenceMessenger.Default.RegisterAll(this);
+
+        TokenHandler = tokenHandler;
+
+        CurrentViewModel = GetCurrentViewModel(tokenHandler);
+    }
+
+    public void Receive(TokenAcquiredEvent message)
+    {
+        CurrentViewModel = GetCurrentViewModel(TokenHandler);
+    }
+
+    private static object GetCurrentViewModel(TokenHandler tokenHandler)
+    {
+        return tokenHandler.HasValidToken() switch
+        {
+            true => App.Services.GetRequiredService<MainViewModel>(),
+            false => App.Services.GetRequiredService<AuthenticationViewModel>()
+        };
+    }
+}
