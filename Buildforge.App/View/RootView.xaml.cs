989c0014500ae4b3b1e49dd3dc669ba69197ef6e
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-09 16:11:12 -0400

    Create RootView.xaml.cs

---
+++ Buildforge.App/View/RootView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Buildforge.App.ViewModel.Root;
+
+namespace Buildforge.App.View;
+
+public partial class RootView : Window
+{
+    public RootView()
+    {
+        InitializeComponent();
+
+        DataContext = App.Services.GetRequiredService<RootViewModel>();
+    }
+}
