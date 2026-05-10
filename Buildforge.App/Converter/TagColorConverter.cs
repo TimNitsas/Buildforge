1be13308a89a6636d84ba958ddf43145e93bb933
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-09 21:50:03 -0400

    Create TagColorConverter.cs

---
+++ Buildforge.App/Converter/TagColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Media;
+
+namespace Buildforge.App.Converter;
+
+public sealed class TagColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not string tag)
+        {
+            return new SolidColorBrush(Colors.DimGray);
+        }
+
+        var color = tag.ToLower() switch
+        {
+            "qa approved" => Color.FromRgb(84, 168, 0),
+            "approved" => Color.FromRgb(84, 168, 0),
+            "buildfix" => Color.FromRgb(87, 168, 0),
+            "hotfix" => Color.FromRgb(168, 157, 0),
+            "custombuild" => Color.FromRgb(0, 84, 168),
+            "rejected" => Color.FromRgb(192, 80, 80),
+            "lab" => Color.FromRgb(80, 120, 192),
+            _ => Colors.DimGray
+        };
+
+        return new SolidColorBrush(color);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
