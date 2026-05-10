da9f3720a9bf7360f25400e775165eee4490a295
Tim <50585813+TimNitsas@users.noreply.github.com>
2026-05-09 21:49:54 -0400

    Create Badge.xaml.cs

---
+++ Buildforge.App/Control/Badge.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Media;
+
+namespace Buildforge.App.Control;
+
+public partial class Badge : UserControl
+{
+    public string Text
+    {
+        get { return (string)GetValue(TextProperty); }
+        set { SetValue(TextProperty, value); }
+    }
+
+    public static readonly DependencyProperty TextProperty = DependencyProperty.Register(nameof(Text), typeof(string), typeof(Badge), new PropertyMetadata(string.Empty));
+
+    public SolidColorBrush Color
+    {
+        get { return (SolidColorBrush)GetValue(ColorProperty); }
+        set { SetValue(ColorProperty, value); }
+    }
+
+    public static readonly DependencyProperty ColorProperty = DependencyProperty.Register(nameof(Color), typeof(SolidColorBrush), typeof(Badge), new PropertyMetadata(new SolidColorBrush()));
+
+    public Badge()
+    {
+        InitializeComponent();
+    }
+}
